5ad7382fa72d2454262f756cf5c8769beffa6df9
robby <robby@LAPTOP-KHUOAICR>
2024-01-25 19:11:00 +0100

    Aggiungere i file di progetto.

---
+++ Quadrato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo r = new Rettangolo();
+            Console.WriteLine("inserure lato 1");
+            r.Lato1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserire lato 2");
+            r.Lato2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserire lato quadrato");
+            r.LatoQuadrato = int.Parse(Console.ReadLine());
+            r.CalcolaAreaPerimetro(r.Lato1,r.Lato2,r.LatoQuadrato);
+            Console.WriteLine("Area e Perimetro Rettangolo:{0}, {1}",r.areaR, r.perimetroR);
+            Console.WriteLine("Area e Perimetro Quadrato: {0}, {1}", r.areaQ, r.perimetroQ);
+            Console.ReadLine();
+     
+        }
+    }
+}
---
+++ Quadrato/Rettangolo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrato
+{
+    internal class Rettangolo
+    {
+        public int Lato1 { get; set; }
+        public int Lato2 { get; set; }
+        public int LatoQuadrato { get; set; }
+        public int areaR { get; set; }
+        public int perimetroR { get; set; }
+        public int areaQ { get; set; }
+        public int perimetroQ { get; set; }
+
+        public void CalcolaAreaPerimetro(int Lato1, int Lato2, int LatoQuadrato) 
+        {
+            areaR = Lato1 * Lato2;
+            perimetroR = 2*Lato2 + 2*Lato1;
+            areaQ = LatoQuadrato * LatoQuadrato;
+            perimetroQ = 4 * LatoQuadrato;
+            
+            
+        }
+    }
+}
